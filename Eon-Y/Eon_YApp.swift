16c3758ad30382c3555b25b4d33dc67030b7a671
TedSvard <Ted@Svardteknik.com>
2026-02-27 22:02:32 +0100

    Initial Commit

---
+++ Eon-Y/Eon_YApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eon_YApp.swift
+//  Eon-Y
+//
+//  Created by Ted Svärd on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct Eon_YApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Eon-YTests/Eon_YTests.swift
@@ -0,0 +1,17 @@
+//
+//  Eon_YTests.swift
+//  Eon-YTests
+//
+//  Created by Ted Svärd on 2026-02-27.
+//
+
+import Testing
+@testable import Eon_Y
+
+struct Eon_YTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
